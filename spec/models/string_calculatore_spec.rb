0b411ba04a2a0008c860ed3f893f7b4674cfa798
Ankit Kushwah <ankitkushwahji159@gmail.com>
2024-06-05 16:40:49 +0530

    Implement feature: returns the number for a single number string

--- spec/models/string_calculatore_spec.rb
+++ spec/models/string_calculatore_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe StringCalculator, type: :model do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+    context "with a single number string" do
+      it "returns 1" do
+        expect(StringCalculator.add("1")).to eq(1)
+      end
+    end
   end
 end
